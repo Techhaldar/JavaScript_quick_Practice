faab800114734b0ed3d33c22d70bcd585171f8c4
Ratin <ratinhaldar123@gmail.com>
2024-09-26 17:52:44 +0530

    Operators and Conditionals

---
+++ Day-3/03_Operators_Conditionals.js
@@ -0,0 +1,48 @@
+// console.log("this is day-3")
+
+// let a = 3;
+// let b = 2;
+
+// Arithmatic operator
+// let sum = a + b;
+// let sub = a - b;
+// let mul = a * b;
+// let div = a / b;
+// let modu = a % b;
+// let pow = a ** b;
+
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(modu);
+// console.log(pow);
+
+// comparison Operator
+
+// == === < > <= >= != !==
+
+// let x = 9;
+// let y = "9";
+
+// console.log(x != y)
+
+// Ternary Operator
+
+// let age = 17;
+
+// let statu = (age >= 18) ? 'I can Vote' : 'I can not Vote';
+
+// console.log(statu);
+
+// Logical Operator
+// && , || , !
+
+// Bitwise Operator
+// AND -> &
+// OR -> |
+// NOT -> ~ 
+// << 
+// >>
+// XOR -> ^
+

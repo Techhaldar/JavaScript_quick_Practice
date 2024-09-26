06f14bfb7f784ee388a3ea3a7e7ff4b14e8f60f5
Ratin <ratinhaldar123@gmail.com>
2024-09-26 19:08:10 +0530

    Loops and Strings

---
+++ Day-4/04_Loops_String.js
@@ -0,0 +1,34 @@
+// for loop
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// while loop
+// let a = 1;
+// while (a < 7) {
+//   if (a == 4) {
+//     a++;
+//     continue;
+//   } else {
+//     console.log(a);
+//   }
+//   a++;
+// } 
+
+// do while loop
+// let a = 1;
+// do {
+//   console.log("ratin")
+//   a++;
+// } while (a <= 5);
+
+
+// String
+// let myName = "ratin";
+// console.log(myName.substring(2,4));
+
+let Sentence = "hii this is ratin haldar";
+let words = Sentence.split(" ");
+
+console.log(typeof (words));
+console.log(words);

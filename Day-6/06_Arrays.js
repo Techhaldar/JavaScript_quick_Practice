cd445e528344bde9dd2d892fafa8a950933d8003
Ratin <ratinhaldar123@gmail.com>
2024-09-27 12:53:21 +0530

    Arrays and methods

---
+++ Day-6/06_Arrays.js
@@ -0,0 +1,59 @@
+// let obj = {
+//   firstName : "Ratin",
+//   lastName : "Haldar",
+//   age : 20,
+//   weight : 62,
+// }
+
+// console.log(typeof(obj));
+
+// Arrays
+
+// let arr = ['ratin', 'raj', 'abhay', 'arjun', 'raju'];
+// let arr1 = ['Haldar', 'singh', 'haldar', 'roy','mallik'];
+
+// console.log(arr);
+// console.log(typeof (arr));
+// console.log(arr[4]);
+
+// console.log(arr.slice(2, 4)) // output abhay, arjun
+
+// let arr = [10, 20, 13, 19, 30, 40, 50]
+
+// console.log(arr.map(e => e*2))
+
+// map mathod
+// console.log(arr.map((Number, index) => {
+//   console.log(Number);
+//   console.log(index);
+// }))
+
+// filter mathod
+
+// console.log(arr.filter((e => e%2 === 0)))
+
+// let arr = [1, 4, 6, 7, 'ratin', 7, true, 0, 'raj', 'abhay', 9]
+
+// console.log(arr.filter((e => e === 'Number'){
+
+// }))
+
+// let arr = [10, 20, 30, 40]
+
+// console.log(arr.reduce((acc, curr) => {
+//   return acc + curr;
+// }, 1))
+
+
+// Home work
+
+// let arr = [9, 1, 7, 4, 2, 8];
+// arr.sort();
+
+// console.log(arr);
+
+let fullName = 'ratinhaldar';
+
+for (const element of fullName) {
+  console.log(element)
+}

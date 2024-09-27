1049c1c12429b8eee27405280c919485830f86bd
Ratin <ratinhaldar123@gmail.com>
2024-09-27 23:18:51 +0530

    Temporal Dead Zone

---
+++ Day-8/08_temporal_Dead.js
@@ -0,0 +1,20 @@
+let num = 18;
+
+console.log(num);
+
+{
+  console.log(num);
+}
+if(true){
+  console.log(num);
+}
+for(let i = 0; i<5; i=i+1){
+  console.log(num)
+}
+
+function myRoll(){
+  console.log("hii this is my roll No.", num);
+}
+console.log(myRoll);
+
+

2f0456f93ce0f41d3e5d8826297f10456dde0079
Ratin <ratinhaldar123@gmail.com>
2024-09-28 14:39:53 +0530

    In Built Objects

---
+++ Day-10/10_In_Built_Objects.js
@@ -0,0 +1,20 @@
+console.log("ratin");
+
+console.log(Math.PI);
+
+console.log(Math.max(1, 2, 5, 20, 100, 16, 78, 199));
+
+console.log(Math.min(90, 20, 5, 1, 10, 4, 5, 100))
+
+console.log(Math.round(5.3));
+
+console.log(Math.floor(1.7))
+
+console.log(Math.ceil(1.7))
+
+let curr = new Date();
+console.log(curr.toDateString());
+console.log(curr.toLocaleDateString());
+
+console.log(curr.getMonth());
+

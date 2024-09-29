21cb51c069749e515a32e2f6b7a53ec0b19d4895
Ratin <ratinhaldar123@gmail.com>
2024-09-29 23:38:29 +0530

    DOM Manipulation and Styling elements

---
+++ Day-14/14_Dom_with_CSS.js
@@ -0,0 +1,14 @@
+let paraElement = document.querySelector("#spara");
+// console.log(paraElement)
+paraElement.style.backgroundColor = 'red';
+
+let firstElement = document.querySelector("#fdiv");
+console.log(firstElement);
+
+firstElement.style.backgroundColor = 'blue';
+
+firstElement.setAttribute('class', 'secondDivClass');
+console.log(firstElement);
+
+
+ 
